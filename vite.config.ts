a8f1cf1fadb820f4c0c59153b6ced8085bdc8cc0
leandrosuy2 <suyninja@gmail.com>
2025-11-06 11:36:02 -0300

    FIx: Ajuste nas configurações de deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    dedupe: ['react', 'react-dom'],
-  },
+  preview: {
+    port: 3001,
+    host: true,
+    allowedHosts: ['todo-todo-frontend.pyxnrg.easypanel.host']
+  }
 })
